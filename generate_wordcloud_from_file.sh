7952f2dd53c960426af306e71897b4fb3380cfe6
Kayley Wong <kayleymanyeewong@gmail.com>
2020-09-03 07:32:54 -0700

    FINAL

--- generate_wordcloud_from_file.sh
+++ generate_wordcloud_from_file.sh
@@ -13,11 +13,15 @@ while read url
 
     done<my_webpages.txt
 
-~/bin/html2text.py current_pages/file1.html > my_current.txt
+PATH_html2text=$(which html2text.py)
 
-~/bin/html2text.py current_pages/file2.html >> my_current.txt
+${PATH_html2text} current_pages/file1.html > my_current.txt
 
-~/.local/bin/wordcloud_cli --text my_current.txt --imagefile wordcloud.png --mask mask_wordcloud-removebg-preview.jpg --background "white" --min_font_size 1 --min_word_length 6 --width 800 --height 400 --margin 1  
+${PATH_html2text} current_pages/file2.html >> my_current.txt
+
+PATH_wordcloud=$(which wordcloud_cli)
+
+${PATH_wordcloud} --text my_current.txt --imagefile wordcloud.png --mask mask_wordcloud-removebg-preview.jpg --background "white" --min_font_size 1 --min_word_length 6 --width 800 --height 400 --margin 1  
 
 cp wordcloud.png ~/public_html
 

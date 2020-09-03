724662e5b41c5e6b0f104fb490ee177b7b5d60f4
Kayley Wong <kayleymanyeewong@gmail.com>
2020-09-03 00:55:18 -0700

    done

---
+++ generate_wordcloud_from_file.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+cd ~/assignments/trgn_wordcloud/
+
+counter=1
+
+while read url
+
+ do
+
+    wget -O ~/assignments/trgn_wordcloud/current_pages/file${counter}.html $url
+
+    ((counter++)) 
+
+done<my_webpages.txt
+
+~/bin/html2text.py current_pages/file1.html > my_current.txt
+
+~/bin/html2text.py current_pages/file2.html >> my_current.txt
+
+~/.local/bin/wordcloud_cli --text my_current.txt --imagefile wordcloud.png --mask mask_wordcloud-removebg-preview.jpg --background "white" --min_font_size 1 --min_word_length 6 --width 800 --height 400 --margin 1  
+
+cp wordcloud.png ~/public_html
+
+echo "Successfully run for $USER"
